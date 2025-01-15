64e81392507264626cedb2d2c1a4e73c154d3d7b
borz7zy <borz7zy@yandex.ru>
2025-01-15 04:39:50 +0100

    adapted for OpenMP

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # samp-json
-A JSON implementation for SA:MP.
+A JSON implementation for SA:MP Open.MP Server.
+
+### Using the plugin
+There are the following native functions:
+```SourcePawn
+native json_decode(json_data[]);
+native json_get_int(id, key[]);
+native bool:json_get_string(id, key[], value[], value_length = sizeof(value));
+native Float:json_get_float(id, key[]);
+native json_delete(id);
+```
+
+can be used as follows:
+```Pawn
+main(){
+    new id_decode = json_decode("{"key_int": 1234, "key_string":"hello, world!", "key_float": 1.0000}");
+    if(id_decode != 0){
+        printf("%d", json_get_int(id_decode, "key_int")); // json_get_int may return -1 if its extraction fails. but -1 may also return as a valid value, make sure your JSON does not use the value -1
+        new str[16];
+        if(json_get_string(id_decode, "key_stirng", str/*, sizeof(str)*/)){
+            printf("%s", str);
+        }
+        printf("%f", json_get_float(id_decode, "key_float")); // json_get_float has a similar situation, it can return -1.0000 if the value is not found. If you are not sure that you do not have -1 - do not use the validity check of the received data
+        json_delete(id_decode); // freeing memory - removing the decoded JSON instance
+    }else{
+        print("Error decode this JSON!"); // we will output a message about unsuccessful JSON decoding
+    }
+}
+```
+
+The JSON passed to json_decode can be loaded from a file, or from any other resource and passed as a string.
--- main.cpp
+++ main.cpp
@@ -1,46 +1,140 @@
-#include <iostream>
+#include <Server/Components/Pawn/pawn.hpp>
+#include <Server/Components/Pawn/Impl/pawn_natives.hpp>
+#include <Server/Components/Pawn/Impl/pawn_impl.hpp>
+
+#include <sdk.hpp>
+
 #include <string>
 #include <vector>
-#include "json_spirit/json_spirit.h"
-#include "amx/amx.h"
-#include "plugincommon.h"
-#include "settings.h"
-#include "AmxUtils.h"
 
-typedef void(*logprintf_t)(char* format, ...);
-logprintf_t logprintf;
-extern void *pAMXFunctions;
+#include "json_spirit/json_spirit.h"
+#include "settings.h"
 
 struct t_JsonData
 {
-    int id;
+    unsigned id;
     json_spirit::Object object;
 };
-
 std::vector<t_JsonData> json_data;
-int json_data_id = 0;
+unsigned json_data_id = 0;
 
-cell AMX_NATIVE_CALL json_decode(AMX* amx, cell* params)
+class SampJson final : public IComponent,
+                       public PawnEventHandler
 {
-    std::string json_string = AmxUtils::amx_GetStdString(amx, &params[1]);
+private:
+    ICore *core_ = nullptr;
+    IPawnComponent *pawn_ = nullptr;
 
+public:
+    PROVIDE_UID(0x151CF6FD9750FEC4);
+
+    SampJson()
+    {
+    }
+    ~SampJson()
+    {
+        if (pawn_)
+        {
+            pawn_->getEventDispatcher().removeEventHandler(this);
+        }
+        if (core_)
+        {
+        }
+    }
+
+    void onAmxLoad(IPawnScript &script) override
+    {
+        pawn_natives::AmxLoad(script.GetAMX());
+    }
+
+    void onAmxUnload(IPawnScript &script) override
+    {
+    }
+
+    StringView componentName() const override
+    {
+        return "SA:MP JSON";
+    }
+
+    SemanticVersion componentVersion() const override
+    {
+        return SemanticVersion(1, 2, 0, 0);
+    }
+
+    void onLoad(ICore *c) override
+    {
+        core_ = c;
+
+        setAmxLookups(core_);
+
+        // core_->printLn("SampJson: succesfully loaded!");
+        core_->printLn(" -----------------------------------------------------------------");
+        core_->printLn(" SA:MP JSON plugin version %s successfully loaded!", PLUGIN_VERSION);
+        core_->printLn(" Developer: Manuel Schnitzer");
+        core_->printLn(" -----------------------------------------------------------------");
+    }
+
+    void onInit(IComponentList *components) override
+    {
+        pawn_ = components->queryComponent<IPawnComponent>();
+
+        if (pawn_)
+        {
+            setAmxFunctions(pawn_->getAmxFunctions());
+            setAmxLookups(components);
+            pawn_->getEventDispatcher().addEventHandler(this);
+        }
+    }
+
+    void onReady() override
+    {
+        // Fire events here at earliest.
+    }
+
+    void onFree(IComponent *component) override
+    {
+        if (component == pawn_)
+        {
+            pawn_ = nullptr;
+            setAmxFunctions();
+            setAmxLookups();
+        }
+        core_->printLn(" * SA:MP JSON plugin was unloaded.");
+    }
+
+    void free() override
+    {
+        // Deletes the component.
+        delete this;
+    }
+
+    void reset() override
+    {
+        // Resets data when the mode changes.
+    }
+};
+
+COMPONENT_ENTRY_POINT()
+{
+    return new SampJson();
+}
+
+SCRIPT_API(json_decode, int(std::string const &json_string))
+{
     json_spirit::Value value;
     auto success = json_spirit::read(json_string, value);
 
     if (success == false)
         return 0;
 
-    json_data_id++;
-    json_data.push_back({ json_data_id, value.get_obj() });
+    ++json_data_id;
+    json_data.push_back({json_data_id, value.get_obj()});
 
     return json_data_id;
 }
 
-cell AMX_NATIVE_CALL json_get_int(AMX* amx, cell* params)
+SCRIPT_API(json_get_int, int(int const data_id, std::string const &key))
 {
-    int data_id = params[1];
-    std::string key = AmxUtils::amx_GetStdString(amx, &params[2]);
-
     for (auto &x : json_data)
     {
         if (x.id == data_id)
@@ -54,15 +148,11 @@ cell AMX_NATIVE_CALL json_get_int(AMX* amx, cell* params)
             }
         }
     }
-
     return -1;
 }
 
-cell AMX_NATIVE_CALL json_get_string(AMX* amx, cell* params)
+SCRIPT_API(json_get_string, bool(int const data_id, std::string const &key, OutputOnlyString &value))
 {
-    int data_id = params[1];
-    std::string key = AmxUtils::amx_GetStdString(amx, &params[2]);
-
     for (auto &x : json_data)
     {
         if (x.id == data_id)
@@ -71,25 +161,20 @@ cell AMX_NATIVE_CALL json_get_string(AMX* amx, cell* params)
             {
                 if (i.name_ == key && i.value_.type() == json_spirit::str_type)
                 {
-                    cell *addr = NULL;
+                    std::string str;
+                    str.assign(i.value_.get_str().c_str());
+                    value = str;
 
-                    amx_GetAddr(amx, params[3], &addr);
-                    amx_SetString(addr, i.value_.get_str().c_str(), 0, 0, params[4]);
-
-                    return 1;
+                    return true;
                 }
             }
         }
     }
-
-    return -1;
+    return false;
 }
 
-cell AMX_NATIVE_CALL json_get_float(AMX* amx, cell* params)
+SCRIPT_API(json_get_float, float(int const data_id, std::string const &key))
 {
-    int data_id = params[1];
-    std::string key = AmxUtils::amx_GetStdString(amx, &params[2]);
-
     for (auto &x : json_data)
     {
         if (x.id == data_id)
@@ -98,73 +183,27 @@ cell AMX_NATIVE_CALL json_get_float(AMX* amx, cell* params)
             {
                 if (i.name_ == key && i.value_.type() == json_spirit::real_type)
                 {
-                    const float value = (float)i.value_.get_real();
-                    return amx_ftoc(value);
+                    return (float)i.value_.get_real();
                 }
             }
         }
     }
 
-    return -1.0;
+    return -1.0f;
 }
 
-cell AMX_NATIVE_CALL json_delete(AMX* amx, cell* params)
+SCRIPT_API(json_delete, bool(int const data_id))
 {
-    int data_id = params[1], index = 0;
-
+    int index = 0;
     for (auto &i : json_data)
     {
         if (i.id == data_id)
         {
-            json_data.erase(json_data.begin()+index);
-            return 1;
+            json_data.erase(json_data.begin() + index);
+            return true;
         }
 
-        index++;
+        ++index;
     }
-
-    return 0;
-}
-
-PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports()
-{
-    return SUPPORTS_VERSION | SUPPORTS_AMX_NATIVES;
-}
-
-PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppData)
-{
-    pAMXFunctions = ppData[PLUGIN_DATA_AMX_EXPORTS];
-    logprintf = (logprintf_t)ppData[PLUGIN_DATA_LOGPRINTF];
-
-    logprintf(" -----------------------------------------------------------------");
-    logprintf(" SA:MP JSON plugin version %s successfully loaded!", PLUGIN_VERSION);
-    logprintf(" Developer: Manuel Schnitzer");
-    logprintf(" -----------------------------------------------------------------");
-
-    return true;
-}
-
-PLUGIN_EXPORT void PLUGIN_CALL Unload()
-{
-    logprintf(" * SA:MP JSON plugin was unloaded.");
-}
-
-extern "C" const AMX_NATIVE_INFO PluginNatives[] =
-{
-    { "json_decode", json_decode },
-    { "json_get_int", json_get_int },
-    { "json_get_string", json_get_string },
-    { "json_get_float", json_get_float },
-    { "json_delete", json_delete },
-    { 0, 0 }
-};
-
-PLUGIN_EXPORT int PLUGIN_CALL AmxLoad(AMX *amx)
-{
-    return amx_Register(amx, PluginNatives, -1);
-}
-
-PLUGIN_EXPORT int PLUGIN_CALL AmxUnload(AMX *amx)
-{
-    return AMX_ERR_NONE;
+    return false;
 }
